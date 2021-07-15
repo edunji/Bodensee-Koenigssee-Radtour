c9c6f05d18b8157ea89a1c29c0ea1edb03f28e53
edunji <87495800+edunji@users.noreply.github.com>
2021-07-15 21:37:58 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const IndexPage = ({
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <HeroHeader/>
-      <h2>Blog Posts &darr;</h2>
+      <h2>Der Radweg Bodensee-Königssee &darr;</h2>
       <div className="grids">
         {Posts}
       </div>
