ee13dfe17ad5c8533bf7fbcdfde6cef944f0c914
edunji <87495800+edunji@users.noreply.github.com>
2021-07-16 16:29:06 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Made with <span role="img" aria-label="love">❤️</span> by Ed & Marie</a></p>
+        <p>&copy; {new Date().getFullYear()} Made with <span role="img" aria-label="love">❤️</span> by Ed & Marie</p>
       </footer>
     </div>
   )
